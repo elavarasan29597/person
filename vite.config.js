acdab759166e093670ed901a79a2aafcc5e8d9a3
Elavarasan Siluvaimanickam <elavarasan.s@ebmpapst.local>
2022-12-23 12:08:06 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  publicPath: "../",
+  publicPath: "./",
+  base: "",
+  build: {
+    outDir: "./docs",
+  },
 });

ac412575bc3e0db552ec9f1b0a5cd88134f84fa8
Elavarasan Siluvaimanickam <elavarasan.s@ebmpapst.local>
2022-12-23 13:01:48 +0530

    dep

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 // import {Vue} from "vue";
 
 const router = createRouter({
-  mode: "hash",
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
+  // history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/login",
